66511baf12063c7611b678498eea1e30f1a8874d
vanessanyc <elm.vanessa.louis@gmail.com>
2023-04-28 14:44:36 -0400

    edits

--- popi/src/components/AlbumDashboard/AlbumDashboard.js
+++ popi/src/components/AlbumDashboard/AlbumDashboard.js
@@ -1,11 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import AlbumInfo from './AlbumInfo';
 import ReviewCard from './ReviewCard';
 import ReviewForm from './ReviewForm';
 import './AlbumDashboard.css';
 
 const AlbumDashboard = () => {
-  
+  // Static album data
+  const album = {
+    coverImageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/51/Igor_-_Tyler%2C_the_Creator.jpg',
+    name: 'Album Name',
+    artist: 'Artist Name',
+    releaseDate: '2023-01-01',
+    genres: ['Pop', 'Rock'],
+    previewUrl: '',
+  };
+
+  // Static reviews data
+  const reviews = [
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'Slaaaaaay!',
+    },
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'SO GOOD!',
+    },
+    {
+      avatarUrl: '',
+      username: 'anon',
+      comment: 'obsessed',
+    },
+  ];
+
+  return (
+    <div className="album-dashboard">
+      <div className="left-side">
+        <AlbumInfo album={album} />
+      </div>
+      <div className="right-side">
+        <div className="reviews-container">
+          {reviews.map((review, index) => (
+            <ReviewCard key={index} review={review} />
+          ))}
+        </div>
+        <ReviewForm />
+      </div>
+    </div>
+  );
 };
 
 export default AlbumDashboard;
--- popi/src/components/AlbumDashboard/ReviewForm.js
+++ popi/src/components/AlbumDashboard/ReviewForm.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const ReviewForm = () => {
-  
+  const [reviewText, setReviewText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // submit the review to backend
+    // resets the textarea after submitting
+    setReviewText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="review-form">
+      <textarea
+        value={reviewText}
+        onChange={(e) => setReviewText(e.target.value)}
+        placeholder="Write a review!"
+      />
+      <button type="submit">Submit Review</button>
+    </form>
+  );
 };
 
 export default ReviewForm;
+
